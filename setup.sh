b6df073f6a14cdc49e295e83eb8167c172259919
teari <businessandschool12345@gmail.com>
2014-06-17 18:00:57 +0900

    言語毎の設定ファイルのリンクを自動で貼る

--- setup.sh
+++ setup.sh
@@ -2,9 +2,21 @@
 
 DOT_FILES=(.zshrc .vimrc .gitignore .screenrc)
 
+VIM_FILES=(python.vim javascript.vim)
+
 for file in ${DOT_FILES[@]}
 do
-	ln -s $HOME/dotfiles/$file $HOME/$file
+    ln -s $HOME/dotfiles/$file $HOME/$file
 done
 
 git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
+
+mkdir after
+mkdir ftplugin
+mv ftplugin after
+mv after ~/.vim
+
+for file in ${VIM_FILES[@]}
+do
+    ln -s $HOME/dotfiles/$file $HOME/.vim/after/ftplugin/$file
+done
