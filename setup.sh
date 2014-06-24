88ad1ccd5e84213d64c02cb0e53d63c936ce4e9d
teari <businessandschool12345@gmail.com>
2014-06-24 10:45:44 +0900

    書き換え(未確認)

--- setup.sh
+++ setup.sh
@@ -7,16 +7,10 @@ VIM_FILES=(python.vim javascript.vim google.vim)
 BIN_FILES=(cpplint.py)
 
 #######ディレクトリ操作#######
-mkdir after
-mkdir ftplugin
-mkdir bin
 git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
-mv bin ~/
-mv ftplugin after
-mv after ~/.vim
 
-rm -rf after
-
+cp  -p after $HOME/.vim/
+cp  -p bin $HOME/
 
 ######リンク######
 for file in ${DOT_FILES[@]}
@@ -24,13 +18,12 @@ do
     ln -s $HOME/dotfiles/$file $HOME/$file
 done
 
-
 for file in ${VIM_FILES[@]}
 do
-    ln -s $HOME/dotfiles/$file $HOME/.vim/after/ftplugin/$file
+    ln  -s $HOME/dotfiles/after/ftplugin/$file $HOME/.vim/after/ftplugin/$file
 done
 
 for file in ${BIN_FILES[@]}
 do
-    ln -s  $HOME/dotfiles/$file $HOME/bin/$file
+    ln  -s $HOME/dotfiles/bin$file $HOME/bin/$file
 done
