f7fd8805a2d635c229605dff0ad25aecfecf55fb
thayakawa <thayakawa@debian>
2018-12-06 21:34:26 -0500

    debian test

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-##!/bin/bash
+#!/bin/bash
 #
 #DOT_FILES=(.zshrc .vimrc .gitignore .gitconfig .screenrc .xvimrc .cheatsheet.md)
 #
@@ -25,58 +25,15 @@
 #ln -snfv $HOME/dotfiles/.vimrc $HOME/.config/nvim/init.vim
 
 
-get_os() {
-  if [ "$(uname)" == 'Darwin' ]; then
-      OS='Mac'
-  elif [ "$(expr substr $(uname -s) 1 5)" == 'Linux' ]; then
-    if  [ -e /etc/debian_version ] ||
-        [ -e /etc/debian_release ]; then
-        # Check Ubuntu or Debian
-        if [ -e /etc/lsb-release ]; then
-          # Ubuntu
-          OS="ubuntu"
-        else
-          # Debian
-          OS="debian"
-        fi
-      elif [ -e /etc/fedora-release ]; then
-        # Fedra
-        OS="fedora"
-      elif [ -e /etc/redhat-release ]; then
-        if [ -e /etc/oracle-release ]; then
-            # Oracle Linux
-            OS="oracle"
-        else
-            # Red Hat Enterprise Linux
-            OS="redhat"
-        fi
-      elif [ -e /etc/arch-release ]; then
-        # Arch Linux
-        OS="arch"
-      elif [ -e /etc/turbolinux-release ]; then
-        # Turbolinux
-        OS="turbol"
-      elif [ -e /etc/SuSE-release ]; then
-        # SuSE Linux
-        OS="suse"
-      elif [ -e /etc/mandriva-release ]; then
-        # Mandriva Linux
-        OS="mandriva"
-      elif [ -e /etc/vine-release ]; then
-        # Vine Linux
-        OS="vine"
-      elif [ -e /etc/gentoo-release ]; then
-        # Gentoo Linux
-        OS="gentoo"
-      else
-        # Other
-        echo "unkown distribution"
-        OS="unkown"
-      fi
-    elif [ "$(expr substr $(uname -s) 1 10)" == 'MINGW32_NT' ]; then
-      OS='Cygwin'
-    else
-      echo "Your platform ($(uname -a)) is not supported."
+function get_os() {
+  if [ $(uname) == 'Darwin' ]; then
+    OS='Mac'
+  elif [ $(uname) == 'Linux' ]; then
+    OS=`cat /etc/os-release | awk -F'["]' 'NR==1{print $2}' | awk '{print $1}'`
+  elif [ $(uname) == 'MINGW32_NT' ]; then
+    OS='Cygwin'
+  else
+    echo "Your platform ($(uname -a)) is not supported."
     exit 1
   fi
   echo ${OS}
